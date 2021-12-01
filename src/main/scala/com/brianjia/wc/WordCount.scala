8b34dd67f1e11e45a1b10b1c2123da60cdf584a5
brianjia2020 <brianjia1123@gmail.com>
2021-11-30 21:11:49 -0500

    add

---
+++ src/main/scala/com/brianjia/wc/WordCount.scala
@@ -0,0 +1,15 @@
+package com.brianjia.wc
+
+import org.apache.flink.api.scala.ExecutionEnvironment
+import org.apache.flink.api.scala._
+
+/**
+ * Batch processing for word count
+ */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    //1. create context
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    //2.
+  }
+}
