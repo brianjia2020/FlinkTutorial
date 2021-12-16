d3931d753a113c741ab41032466b464c307e075b
brianjia2020 <brianjia1123@gmail.com>
2021-12-15 20:59:49 -0500

    add

--- src/main/scala/com/brianjia/wc/StreamWordCount.scala
+++ src/main/scala/com/brianjia/wc/StreamWordCount.scala
@@ -1,5 +1,6 @@
 package com.brianjia.wc
 
+import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.scala._
 
@@ -10,9 +11,15 @@ object StreamWordCount {
   def main(args: Array[String]): Unit = {
     //1. create context, same as batch processing
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+//    env.setParallelism(8)
+    //read configuration files as input params
+    val paramTool: ParameterTool = ParameterTool.fromArgs(args)
+    val host: String = paramTool.get("host")
+    val port: Int = paramTool.getInt("port")
 
+//    print(paramTool.toMap)
     //2. receive a web socket text stream
-    val inputDataStream: DataStream[String] = env.socketTextStream("localhost",7777)
+    val inputDataStream: DataStream[String] = env.socketTextStream(host,port)
     // transformation and statistics
 
     val resultDataStream: DataStream[(String, Int)] = inputDataStream
