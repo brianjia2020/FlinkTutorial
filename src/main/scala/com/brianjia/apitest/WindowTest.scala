9e37437e4b53e9d9da5644dc40ecd2bddb3c8deb
brianjia2020 <brianjia1123@gmail.com>
2022-02-11 09:23:57 -0500

    now

--- src/main/scala/com/brianjia/apitest/WindowTest.scala
+++ src/main/scala/com/brianjia/apitest/WindowTest.scala
@@ -1,27 +1,41 @@
 package com.brianjia.apitest
 
+import org.apache.flink.api.common.eventtime.WatermarkStrategy
 import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
-import org.apache.flink.streaming.api.windowing.assigners.{EventTimeSessionWindows, SlidingEventTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
+import org.apache.flink.streaming.runtime.operators.util.AssignerWithPeriodicWatermarksAdapter
 
 object WindowTest {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
-    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+    env.getConfig.setAutoWatermarkInterval(500)
 
 //    val inputPath = "/Users/chunyangjia/Desktop/self_study/FlinkTutorial/src/main/resources/sensor.txt"
 //    val inputStream: DataStream[String] = env.readTextFile(inputPath)
 
     val inputStream = env.socketTextStream("localhost", 7777)
     //1. convert to sensor reading format
-    val dataStream = inputStream.map(
-      data => {
-        val arr = data.split(",")
-        SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
-      }
-    )
+    val dataStream = inputStream
+      .map(
+        data => {
+          val arr = data.split(",")
+          SensorReading(arr(0), arr(1).toLong, arr(2).toDouble)
+        }
+      )
+//      .assignAscendingTimestamps(_.timestamp * 1000L)
+      // delay as 3s and time as the input event time
+      // 3s is a guess, you need to adjust based on different realistic scenarios
+      .assignTimestampsAndWatermarks(new BoundedOutOfOrdernessTimestampExtractor[SensorReading](Time.seconds(3)) {
+        override def extractTimestamp(t: SensorReading): Long = t.timestamp * 1000L;
+      })
+
+    val lateTag = new OutputTag[(String, Double, Long)]("late")
+
+//      .assignTimestampsAndWatermarks(WatermarkStrategy[SensorReading])
 
     // count every sensor's min temp every 15s
     val resultStream = dataStream
@@ -31,11 +45,14 @@ object WindowTest {
 //      .window(SlidingEventTimeWindows.of(Time.seconds(15), Time.seconds(3))) // sliding window
 //      .window(EventTimeSessionWindows.withGap(Time.seconds(10)))
       .timeWindow(Time.seconds(15))
+      .allowedLateness(Time.seconds(60))
+      .sideOutputLateData(lateTag)
 //      .countWindow(1000)
 //      .minBy(1)
       .reduce((curRes, newData) => (curRes._1, curRes._2.min(newData._2), newData._3))
 
-    resultStream.print()
+    resultStream.getSideOutput(lateTag).print("late")
+    resultStream.print("result")
 
     env.execute("min")
   }
